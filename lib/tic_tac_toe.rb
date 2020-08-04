bc00857fa7fb959074edae28e00ecf7faa248c2e
Mike Esposito <mikeesposito6852@gmail.com>
2020-08-04 13:42:52 -0700

    Edits from Rubocop

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,38 +1,40 @@
+# frozen_string_literal: true
+
 # A command line implementation of the classic game, using classes
 
+# for playing the actual game
 class Game
-
   def initialize
-    @current_player = ""
+    @current_player = ''
     @board = Board.new
   end
 
   def play_game
-    get_first_player
+    determine_first_player
     puts declare_winner(play_turns)
   end
- 
+
   # get a valid first player from the user
-  def get_first_player
-    puts "Who is going first, X or O?"
+  def determine_first_player
+    puts 'Who is going first, X or O?'
     first_player = gets.chomp.upcase
-    while first_player != "X" && first_player != "O"
-      puts "Please enter X or O"
+    while first_player != 'X' && first_player != 'O'
+      puts 'Please enter X or O'
       first_player = gets.chomp.upcase
     end
     @current_player = first_player
   end
-  
+
   # while the game is not over, play a turn:
   # display the board, get the current player's move, display the new board
   def play_turns
     counter = 0
-    while (@board.determine_winner != "X" && @board.determine_winner != "O" && counter < 9)
+    while @board.determine_winner != 'X' && @board.determine_winner != 'O' && counter < 9
       puts "It's #{@current_player}'s turn!"
       @board.display
       process_input(@current_player)
       @board.display
-      @current_player == "X" ? @current_player = "O" : @current_player = "X"
+      @current_player = @current_player == 'X' ? 'O' : 'X'
       counter += 1
     end
     counter
@@ -41,16 +43,16 @@ class Game
   # at the end of the game, display the game status (winner or "it's a tie")
   def declare_winner(counter)
     if counter == 9
-      @board.determine_winner == "no winner" ? "It's a tie!" : "#{@board.determine_winner} wins!"
+      @board.determine_winner == :no_winner ? "It's a tie!" : "#{@board.determine_winner} wins!"
     else
       "#{@board.determine_winner} wins!"
     end
   end
-  
+
   # check that the input is an integer between 1 and 9
   def correct_number(input)
-    while !input.between?(1,9)
-      puts "Please enter an integer between 1 and 9"
+    until input.between?(1, 9)
+      puts 'Please enter an integer between 1 and 9'
       input = gets.chomp.to_i
     end
     input
@@ -59,7 +61,7 @@ class Game
   # check that the input has not been played yet
   def not_played_yet(input)
     while @board.occupied?(input)
-      puts "Please enter an integer that has not been played yet"
+      puts 'Please enter an integer that has not been played yet'
       input = gets.chomp.to_i
     end
     input
@@ -67,22 +69,21 @@ class Game
 
   # get a proper move from the current player, and store it in the board
   def process_input(user)
-    puts "Select one of the available slots"
+    puts 'Select one of the available slots'
     input = not_played_yet(correct_number(gets.chomp.to_i))
     @board.populate_square(user, input)
   end
-
 end
 
+# the representation of the game board
 class Board
-
   def initialize
-    @grid = [1,2,3,4,5,6,7,8,9]
+    @grid = [1, 2, 3, 4, 5, 6, 7, 8, 9]
   end
 
   # check if a board square already has an X or an O
   def occupied?(input)
-    @grid[input - 1] == "X" || @grid[input - 1] == "O"
+    @grid[input - 1] == 'X' || @grid[input - 1] == 'O'
   end
 
   # populate a board square
@@ -109,7 +110,7 @@ class Board
         end
       end
     end
-    return "no winner"
+    :no_winner
   end
 
   # display the board
@@ -136,7 +137,6 @@ class Board
 
     HEREDOC
   end
-  
 end
 
 game = Game.new
